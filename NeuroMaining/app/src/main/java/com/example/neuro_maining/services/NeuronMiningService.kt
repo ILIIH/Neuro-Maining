c79691ca3eed4210a7007ad979b0d2eb7325c08e
illiabranchuk <jointlord0@gmail.com>
2024-12-16 20:50:21 +0000

    socket tests

--- NeuroMaining/app/src/main/java/com/example/neuro_maining/services/NeuronMiningService.kt
+++ NeuroMaining/app/src/main/java/com/example/neuro_maining/services/NeuronMiningService.kt
@@ -1,4 +1,5 @@
 package com.example.neuro_maining.services
+
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
@@ -7,6 +8,13 @@ import android.content.Intent
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
 import com.example.neuro_maining.R
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.net.ServerSocket
+import java.net.Socket
+
 
 class NeuronMiningService : Service() {
 
@@ -37,6 +45,53 @@ class NeuronMiningService : Service() {
         startForeground(1, notification)
     }
 
+    private fun setUpServerSocket(){
+        Thread {
+            try {
+                val serverSocket =
+                    ServerSocket(8888) // Choose a port
+                val client =
+                    serverSocket.accept() // Wait for the client to connect
+
+                // Read data from the client
+                val inputStream = client.getInputStream()
+                val reader =
+                    BufferedReader(InputStreamReader(inputStream))
+                val message = reader.readLine() // Example: reading a string
+                println("Received: $message")
+
+                // Close connections
+                reader.close()
+                client.close()
+                serverSocket.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }.start()
+    }
+
+    private fun setUpClientSocket(){
+        Thread {
+            try {
+                val socket = Socket(
+                    "192.168.49.1",
+                    8888
+                )
+
+                // Send data to the server
+                val outputStream = socket.getOutputStream()
+                val writer = PrintWriter(outputStream, true)
+                writer.println("Hello, server!")
+
+                // Close connection
+                writer.close()
+                socket.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }.start()
+
+    }
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
