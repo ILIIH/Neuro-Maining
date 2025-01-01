a54b6eb26346629ab9b4a2c846919fc34b562bf3
illiabranchuk <jointlord0@gmail.com>
2025-01-01 16:21:15 +0000

    small UI fix

--- NeuroMaining/app/src/main/java/com/example/neuro_maining/ui/custom_view/PlotView.kt
+++ NeuroMaining/app/src/main/java/com/example/neuro_maining/ui/custom_view/PlotView.kt
@@ -1,7 +1,6 @@
 package com.example.neuro_maining.ui.custom_view
 
 import android.graphics.Paint
-import android.util.Log
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -11,15 +10,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PaintingStyle
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.DrawContext
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.nativeCanvas
-import kotlinx.coroutines.yield
 import java.util.Calendar
 
 const val PLOT_MARGIN = 50f
 const val AXIS_FONT_SIZE = 30f
+const val PLOT_WITH_FACTOR = 1.1f
 
 @Composable
 fun PlotView(points: List<Pair<Int, Float>>) {
@@ -47,7 +47,7 @@ fun PlotView(points: List<Pair<Int, Float>>) {
         // Axis path: X-axis and Y-axis lines
         axesPath.apply {
             moveTo(PLOT_MARGIN, canvasHeight)
-            lineTo(canvasWidth, canvasHeight)
+            lineTo(canvasWidth/PLOT_WITH_FACTOR, canvasHeight)
 
             moveTo(PLOT_MARGIN, PLOT_MARGIN)
             lineTo(PLOT_MARGIN, canvasHeight)
@@ -72,11 +72,10 @@ fun PlotView(points: List<Pair<Int, Float>>) {
     fun drawPoints(canvasHeight: Float, canvasWidth: Float, drawContext:DrawContext){
         val currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
         val hours = (0..currentHour).toList()
-        val hourSpacing = canvasWidth / hours.size - PLOT_MARGIN / (hours.size + 1)
+        val hourSpacing = (canvasWidth / hours.size - PLOT_MARGIN / (hours.size + 1))/PLOT_WITH_FACTOR
 
         earningPath.moveTo(PLOT_MARGIN, canvasHeight)
         val maxValue = points.maxBy { it.second}
-        Log.i("Drawing_logging","points = ${points}")
 
         hours.forEachIndexed { index, hour ->
             val x = hourSpacing * index
@@ -93,12 +92,11 @@ fun PlotView(points: List<Pair<Int, Float>>) {
                 6f,
                 paint
             )
-            Log.i("Drawing_logging","x = ${x}")
             val n = points.find { it.first == index}
             if(n!=null){
                 val y = (  canvasHeight * n.second - canvasHeight) / maxValue.second
                 earningPath.apply {
-                    lineTo(x, y)
+                    lineTo(x+ PLOT_MARGIN, y)
                     drawContext.canvas.nativeCanvas.drawCircle(
                         x + PLOT_MARGIN,
                         y,
@@ -114,8 +112,11 @@ fun PlotView(points: List<Pair<Int, Float>>) {
             path = earningPath,
             androidx.compose.ui.graphics.Paint().apply {
                 color = Color.Black
-            strokeWidth = 12.0f}
+                strokeWidth = 10.0f
+                style = PaintingStyle.Stroke
+            }
         )
+
     }
     Box(modifier = Modifier.fillMaxSize()) {
         Canvas(modifier = Modifier.fillMaxHeight(0.3f).fillMaxWidth(1f)) {
