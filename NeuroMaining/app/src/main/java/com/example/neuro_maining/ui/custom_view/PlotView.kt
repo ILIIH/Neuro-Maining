63e13cc07d609972045c047ce2713244f9174359
illiabranchuk <jointlord0@gmail.com>
2025-01-14 21:12:16 +0000

    canvas change

--- NeuroMaining/app/src/main/java/com/example/neuro_maining/ui/custom_view/PlotView.kt
+++ NeuroMaining/app/src/main/java/com/example/neuro_maining/ui/custom_view/PlotView.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PaintingStyle
 import androidx.compose.ui.graphics.drawscope.DrawContext
 import androidx.compose.ui.graphics.nativeCanvas
@@ -38,6 +39,11 @@ fun PlotView(points:List<MiningHistory>, modifier: Modifier = Modifier) {
         strokeWidth = 8f
     }
 
+    val graphPaint = Paint().apply {
+        color = android.graphics.Color.BLACK
+        style = Paint.Style.STROKE
+        strokeWidth = 8f
+    }
     fun configurePaths(canvasWidth: Float, canvasHeight: Float) {
         axesPath.reset()
         axesPath.apply {
@@ -49,7 +55,67 @@ fun PlotView(points:List<MiningHistory>, modifier: Modifier = Modifier) {
         }
     }
 
-    fun drawPoints(canvasHeight: Float, canvasWidth: Float, drawContext:DrawContext){
+    fun calculateY(maxValue: Float, canvasHeight: Float, realY : Float) = ( maxValue *PLOT_MARGIN)/canvasHeight+ canvasHeight -  (canvasHeight *  realY )/maxValue
+
+    fun drawGraph(canvasHeight: Float, hours: List<Int>, hourSpacing: Float, maxValue:Float, drawContext:DrawContext){
+        for(poinsPool in points){
+            val earningPath = Path()
+            earningPath.moveTo(PLOT_MARGIN, canvasHeight)
+
+            hours.forEachIndexed { index, hour ->
+                val x = hourSpacing * index
+                val n = poinsPool.miningResults.find { it.first == index}
+                if(n!=null){
+                    val y = calculateY(maxValue = maxValue, canvasHeight = canvasHeight, realY = n.second)
+                    earningPath.apply {
+                        lineTo(x+ PLOT_MARGIN, y)
+                    }
+                }
+            }
+
+            drawContext.canvas.nativeCanvas.drawPath(
+                earningPath,
+                Paint().apply {
+                    color = poinsPool.color.copy(alpha = 0.5f).toArgb()
+                    strokeWidth = 10.0f
+                    style = Paint.Style.STROKE
+                }
+            )
+
+        }
+    }
+
+    fun drawPoints(canvasHeight: Float, hours: List<Int>, hourSpacing: Float, maxValue:Float, drawContext:DrawContext){
+        for(poinsPool in points) {
+            hours.forEachIndexed { index, hour ->
+                val x = hourSpacing * index
+                val n = poinsPool.miningResults.find { it.first == index}
+                if(n!=null){
+                    val y = calculateY(maxValue = maxValue, canvasHeight = canvasHeight, realY = n.second)
+                    drawContext.canvas.nativeCanvas.drawCircle(
+                        x + PLOT_MARGIN,
+                        y,
+                        14f,
+                        graphPaint.apply {
+                            color = poinsPool.color.copy(alpha = 0.5f).toArgb()
+                        }
+                    )
+                    drawContext.canvas.nativeCanvas.drawCircle(
+                        x + PLOT_MARGIN,
+                        y,
+                        8f,
+                        graphPaint.apply {
+                            color = Color.White.toArgb()
+                            style = Paint.Style.FILL
+                        }
+                    )
+                }
+            }
+
+        }
+    }
+
+    fun onDraw(canvasHeight: Float, canvasWidth: Float, drawContext:DrawContext){
         val currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
         val hours = (0..currentHour).toList()
         val hourSpacing = (canvasWidth / hours.size - PLOT_MARGIN / (hours.size + 1))/PLOT_WIDTH_FACTOR
@@ -70,43 +136,28 @@ fun PlotView(points:List<MiningHistory>, modifier: Modifier = Modifier) {
                 textPaint
             )
             tempMaxValue -= canvasHeight/earningStep
-            drawContext.canvas.nativeCanvas.drawCircle(
-                PLOT_MARGIN,
+            drawContext.canvas.nativeCanvas.drawLine(
+                PLOT_MARGIN - PLOT_MARGIN/4,
                 i.toFloat(),
-                6f,
+                PLOT_MARGIN+PLOT_MARGIN/4,
+                i.toFloat(),
                 axesPaint
             )
         }
 
-        for(poinsPool in points){
-            val earningPath = Path()
-            earningPath.moveTo(PLOT_MARGIN, canvasHeight)
-
-            hours.forEachIndexed { index, hour ->
-                val x = hourSpacing * index
-                val n = poinsPool.miningResults.find { it.first == index}
-                if(n!=null){
-                    val y = ( maxValue *PLOT_MARGIN)/canvasHeight+ canvasHeight -  (canvasHeight *  n.second )/maxValue
-                    earningPath.apply {
-                        lineTo(x+ PLOT_MARGIN, y)
-                        drawContext.canvas.nativeCanvas.drawCircle(
-                            x + PLOT_MARGIN,
-                            y,
-                            14f,
-                            axesPaint
-                        )
-                    }
-                }
-            }
-            drawContext.canvas.nativeCanvas.drawPath(
-                earningPath,
-                Paint().apply {
-                    color = poinsPool.color.copy(alpha = 0.5f).toArgb()
-                    strokeWidth = 10.0f
-                    style = Paint.Style.STROKE
-                }
+        drawGraph(canvasHeight = canvasHeight,
+            hours = hours,
+            hourSpacing = hourSpacing,
+            maxValue = maxValue,
+            drawContext = drawContext
             )
-        }
+
+        drawPoints(canvasHeight = canvasHeight,
+            hours = hours,
+            hourSpacing = hourSpacing,
+            maxValue = maxValue,
+            drawContext = drawContext
+        )
 
         hours.forEachIndexed { index, hour ->
             val x = hourSpacing * index
@@ -126,11 +177,13 @@ fun PlotView(points:List<MiningHistory>, modifier: Modifier = Modifier) {
             )
         }
     }
+
+
     Box(modifier = modifier.fillMaxSize()) {
         Canvas(modifier = Modifier.fillMaxHeight(0.3f).fillMaxWidth(1f)) {
             configurePaths(size.width, size.height)
             drawContext.canvas.nativeCanvas.drawPath(axesPath , axesPaint)
-            drawPoints(size.height, size.width, drawContext)
+            onDraw(size.height, size.width, drawContext)
         }
     }
 }
