3ef316f888998434d15c502b26a1f6a0c3676278
illiabranchuk <jointlord0@gmail.com>
2025-01-17 17:49:00 +0000

    changed  earnings list

--- NeuroMaining/app/src/main/java/com/example/neuro_maining/ui/earnings_screen/EarningsList.kt
+++ NeuroMaining/app/src/main/java/com/example/neuro_maining/ui/earnings_screen/EarningsList.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -54,38 +55,39 @@ fun EarningsList(modifier: Modifier) {
             )
             .height(250.dp),
         contentPadding = PaddingValues(8.dp),
-        verticalArrangement = Arrangement.spacedBy(24.dp)
+        verticalArrangement = androidx.compose.foundation.layout.Arrangement.Center,
     ) {
         item {
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(10.dp)
+                    .padding(bottom = 40.dp),
+                contentAlignment = Alignment.Center
             ) {
                 Text(
                     text = "Tasks earning",
                     style = MaterialTheme.typography.bodyLarge,
-                    modifier = Modifier.align(Alignment.Center)
                 )
             }
         }
         items(
             count = miningHistory.size,
             itemContent = { index ->
-                ListItem(miningHistory[index])
+                EarningListItem(miningHistory[index])
             }
         )
         item {
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
+                    .padding(top = 40.dp),
+                contentAlignment = Alignment.Center
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.ic_more),
                     contentDescription = "See more icon",
-                    modifier = Modifier
-                        .size(30.dp)
-                        .align(Alignment.Center),
+                    modifier = Modifier.size(30.dp),
                 )
             }
         }
@@ -93,10 +95,11 @@ fun EarningsList(modifier: Modifier) {
 }
 
 @Composable
-fun ListItem(item: MiningHistory) {
+fun EarningListItem(item: MiningHistory) {
     Card(
         modifier = Modifier
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .padding(10.dp),
         colors = CardDefaults.cardColors(containerColor = Color.Transparent)
     ) {
         Row(
@@ -104,6 +107,17 @@ fun ListItem(item: MiningHistory) {
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically
         ) {
+            Box(
+                modifier = Modifier
+                    .size(20.dp)
+                    .background(item.color, shape = CircleShape)
+                    .border(
+                        width = 2.dp,
+                        color = Color.Black,
+                        shape = CircleShape
+                    )
+            )
+            Spacer(modifier = Modifier.width(40.dp))
             Text(
                 text = item.miningSource,
                 style = MaterialTheme.typography.bodyLarge.copy(
@@ -112,16 +126,10 @@ fun ListItem(item: MiningHistory) {
             )
             Spacer(modifier = Modifier.width(40.dp))
             Text(
-                text = "${item.getEarningSum()* EARNING_MULTIPLIER} $",
+                text = "${item.getEarningSum()* EARNING_MULTIPLIER}",
                 style = MaterialTheme.typography.bodyLarge.copy(
                     fontWeight = FontWeight.Bold
                 )
-            )
-            Spacer(modifier = Modifier.width(40.dp))
-            Box(
-                modifier = Modifier
-                    .size(40.dp)
-                    .background(item.color, shape = RoundedCornerShape(8.dp))
             )
         }
     }
